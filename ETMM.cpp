62f4254791120511868ae3e47221b356170f15e2
Lyuben Davidov <ljubo.davidov@gmail.com>
2024-10-08 16:19:15 +0200

    Added Omega Matrix

--- ETMM.cpp
+++ ETMM.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <cmath>
+#include <complex>
+#include <Eigen/Dense>
 #include <vector>
+
+using namespace std;
 
 // Define constants
 const double PI   = 3.141592653589793;
@@ -22,9 +26,29 @@ double eps_r1 = pow(n1, 2);
 double eps_r2 = pow(n2, 2);
 
 
+// Define 4x4 Complex Valued Matrix Layer 1
+Eigen::Matrix<complex<double>, 4, 4> Omega_L1
+{
+    {complex<double>(0,0), complex<double>(0,0), complex<double>(0,0), complex<double>(1,0)},
+    {complex<double>(0,0), complex<double>(0,0), complex<double>(-1,0), complex<double>(0,0)},
+    {complex<double>(0,0), complex<double>(eps_r1,0), complex<double>(0,0), complex<double>(0,0)},
+    {complex<double>(-eps_r1,0), complex<double>(0,0), complex<double>(0,0), complex<double>(0,0)}
+};
+
+
+// Define 4x4 Complex Valued Matrix Layer 2
+Eigen::Matrix<complex<double>, 4, 4> Omega_L2
+{
+    {complex<double>(0,0), complex<double>(0,0), complex<double>(0,0), complex<double>(1,0)},
+    {complex<double>(0,0), complex<double>(0,0), complex<double>(-1,0), complex<double>(0,0)},
+    {complex<double>(0,0), complex<double>(eps_r2,0), complex<double>(0,0), complex<double>(0,0)},
+    {complex<double>(-eps_r2,0), complex<double>(0,0), complex<double>(0,0), complex<double>(0,0)}
+};
+
+
 // Define the linspace function
-std::vector<double> linspace(double start, double end, int num) {
-    std::vector<double> result;
+vector<double> linspace(double start, double end, int num) {
+    vector<double> result;
     if (num == 0) return result; // Handle case where num is 0
     if (num == 1) {
         result.push_back(start);  // If num is 1, just return the start value
@@ -43,16 +67,16 @@ int main(){
 
     // HERE STARTS MY LINSPACE TEST CODE
     // Example usage: generate 11 values between 1500 and 1600
-    //std::vector<double> arr = linspace(1500, 1600, 11);
+    //vector<double> arr = linspace(1500, 1600, 11);
 
     // Print the array to confirm the values
     //for (double value : arr) {
-    //    std::cout << value << " ";
+    //    cout << value << " ";
     //}
-    //std::cout << std::endl;
+    //cout << endl;
     // HERE ENDS MY LINSPACE TEST CODE
 
-    std::cout << k0 << std::endl;
+    std::cout << Omega_L2 << std::endl;
 
     return 0;
 }
